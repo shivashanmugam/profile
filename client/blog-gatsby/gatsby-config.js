1bd6e2e66e74ee2a8f2d7b4469f6862102c89674
shivashanmugam <sivashanmugam4444@gmail.com>
2021-11-12 18:33:05 +0530

    adding gatsby blog

---
+++ client/blog-gatsby/gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `My Blog Title`,
+    author: `My Name`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-theme-blog-darkmode`,
+      options: {},
+    },
+  ],
+}
