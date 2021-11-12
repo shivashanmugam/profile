82578dc5b033bdf0316490c32238eca1d0e3a254
shivashanmugam <sivashanmugam4444@gmail.com>
2021-11-12 21:01:15 +0530

    code update

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/karthi', (req, res) => res.sendFile('./pages/karthi.html'))
 app.get(`/client`, (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+app.get(`/blog`, (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/blog/public/index.html'));
+});
 
 var filePath = {
     public: {
@@ -20,10 +23,12 @@ var filePath = {
 
 app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '/pages'));
+app.use(express.static('client/blog/public'))
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use('/static', express.static(path.join(__dirname, '/client/build/static')))
+app.use('/blog', express.static(path.join(__dirname, '/client/blog/public/index.html')))
 
 
 
