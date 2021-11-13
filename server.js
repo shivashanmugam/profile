0698b88fc334dea908bf849f162bb6b47d16c541
shivashanmugam <sivashanmugam4444@gmail.com>
2021-11-13 19:46:56 +0530

    Server Updating server code

--- server.js
+++ server.js
@@ -3,15 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express()
 const port = 3005
-// page configuration
-app.get('/', (req, res) => res.sendFile('./pages/home.html'))
-app.get('/karthi', (req, res) => res.sendFile('./pages/karthi.html'))
-app.get(`/client`, (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
-app.get(`/blog`, (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/blog/public/index.html'));
-});
+
 
 var filePath = {
     public: {
@@ -23,13 +15,20 @@ var filePath = {
 
 app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '/pages'));
-app.use(express.static('client/blog/public'))
+
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.use('/static', express.static(path.join(__dirname, '/client/build/static')))
-app.use('/blog', express.static(path.join(__dirname, '/client/blog/public/index.html')))
+// app.use('/static', express.static(path.join(__dirname, '/client/build/static')))
+// app.use(express.static(path.join(__dirname, '/client/build')));
+// app.use(express.static('client/blog/public'))
 
-
+// page configuration
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/pages/home.html')))
+app.get(`/client`, (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+app.get(`/blog`, (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/blog/public/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
