dd5204e4b9f02e8cec25e4f40af87dc4d6a4c991
shivashanmugam <sivashanmugam4444@gmail.com>
2021-11-29 14:40:58 +0530

    Adding blog public folder as part codebase

---
+++ client/blog/public/component---src-pages-subscribe-js-edd74a43d6e89b74762b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{o5uR:function(n,t,e){"use strict";e.r(t),e.d(t,"pageQuery",(function(){return c}));e("q1tI");var o=e("7oih"),r=e("VUD3"),u=e("wvGK"),a=e("qKvR");t.default=function(n){var t=n.data.site;return Object(a.d)(o.a,{site:t,noFooter:!0,noSubscribeForm:!0},Object(a.d)(r.a,null,Object(a.d)(u.a,null)))};var c="1256710295"}}]);
+//# sourceMappingURL=component---src-pages-subscribe-js-edd74a43d6e89b74762b.js.map
