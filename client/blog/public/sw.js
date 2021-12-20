d6d20bd54c312763d3e2dfeed08923d67da06dfc
shivashanmugam <sivashanmugam4444@gmail.com>
2021-12-20 14:04:50 +0530

    code update after theme update

--- client/blog/public/sw.js
+++ client/blog/public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6bbc0bc4bf95c4ebcfab.js"
+    "url": "webpack-runtime-87c76df1fef384e65ebf.js"
   },
   {
     "url": "framework-5c4caac0b3c7889bf4e2.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5ca1b1ca202378c90d7459f08488b2b2"
+    "revision": "d745b51e49b1405a5ad06a60c6ca6d09"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b3d32bc20641419cfed7.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "74c70419c4a1f3c9cdc6c5bc7515fc00"
+    "revision": "0e4422012e2bc5789657aacc2f1f12c3"
   },
   {
     "url": "manifest.webmanifest",
